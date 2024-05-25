2954e2d6d42bf7b0359e424103dcad48f5836def
VanNT <nguyentienvan1991@gmail.com>
2024-05-25 18:25:49 +0700

    chore: update github url

--- packages/host/index.js
+++ packages/host/index.js
@@ -11,7 +11,8 @@ import {version as appVersion} from './package.json';
 
 ScriptManager.shared.addResolver(async (scriptId, caller) => {
   const containersURL = getContainersURL({
-    hostname: process.env.SAS_CATALOG_SERVER_URL,
+    // hostname: process.env.SAS_CATALOG_SERVER_URL,
+    hostname: 'https://super-app-showcase-drab.vercel.app',
     version: appVersion,
     platform: Platform.OS,
     appName,
